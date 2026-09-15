980df6c02faeea4fb7fdb94b526a4ca5a339ca75
David Tapia <datapiao@puce.edu.ec>
2026-09-15 15:51:19 -0500

    initial Commit: Proyecto base estandarizado

---
+++ app/src/main/java/com/example/rda1_laboratorio2_davidtapia/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.rda1_laboratorio2_davidtapia
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rda1_laboratorio2_davidtapia.ui.theme.RDA1_Laboratorio2_DavidTapiaTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RDA1_Laboratorio2_DavidTapiaTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Bienvenido al curso 2026, $name",
+        color = Color.Blue,
+        style = MaterialTheme.typography.headlineMedium
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RDA1_Laboratorio2_DavidTapiaTheme {
+        Greeting("Android")
+    }
+}
